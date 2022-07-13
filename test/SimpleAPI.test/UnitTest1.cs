9a48c7fa9bcc8e981b0854859b950babf7e0e224
Luis Ponciano <luisp033@gmail.com>
2022-07-13 22:11:13 +0100

    Initial Commit

---
+++ test/SimpleAPI.test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.test;
+
+public class UnitTest1
+{
+
+    WeatherForecastController controller = new WeatherForecastController(null);
+    [Fact]
+    public void GetReturnsMyName()
+    {
+        var returnValue = controller.Get();
+        Console.WriteLine(returnValue);
+        Assert.NotNull(returnValue);
+    }
+
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
